ff3f6493933a98548ffc39bece0aea856e9f5903
norakurage <stove2357@outlook.jp>
2026-04-23 00:05:45 +0900

    index.jsとpackage.jsonの修正

--- recipe-tree-app/index.js
+++ recipe-tree-app/index.js
@@ -1,4 +1,13 @@
-const port = process.env.PORT || 8080; // これが重要！
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end('Soulmask Recipe Tree が起動しました！');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
